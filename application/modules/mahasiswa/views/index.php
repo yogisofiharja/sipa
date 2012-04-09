be6351f1d5a91ec3da5291f098e14de19b5a8865
yogi sofi harja <hardja.ogie@yahoo.com>
2012-04-09 15:49:00 +0700

    Update application/modules/mahasiswa/views/index.php

--- application/modules/mahasiswa/views/index.php
+++ application/modules/mahasiswa/views/index.php
@@ -6,11 +6,11 @@
 <?php foreach ($list_mahasiswa as $mahasiswa) : ?>
 	<li>
 		<div id="item_mahasiswa">
-			<h3><?= $mahasiswa->nama ?></h3>
+			<h3><?php echo $mahasiswa->nama ?></h3>
 			<a href="<?php echo site_url('mahasiswa/showFormUpdatemahasiswa/'.$mahasiswa->id_mahasiswa);?>">Edit</a> | 
 			<a href="<?php echo site_url('mahasiswa/deletemahasiswa/'.$mahasiswa->id_mahasiswa);?>">Delete</a> <br><br>
-			nip : <?= $mahasiswa->nim ?> <br>
-			alamat : <?= $mahasiswa->alamat ?> <br>
+			nip : <?php echo $mahasiswa->nim ?> <br>
+			alamat : <?php echo $mahasiswa->alamat ?> <br>
 		</div>
 	</li>
 <?php endforeach;?>
