1bcb7d116d16e9bb75a02a9dcc1db54e2e772299
yogi sofi harja <hardja.ogie@yahoo.com>
2012-04-09 15:47:59 +0700

    Update application/modules/dosen/views/index.php

--- application/modules/dosen/views/index.php
+++ application/modules/dosen/views/index.php
@@ -6,11 +6,11 @@
 <?php foreach ($list_dosen as $dosen) : ?>
 	<li>
 		<div id="item_dosen">
-			<h3><?= $dosen->nama ?></h3>
+			<h3><?php echo $dosen->nama ?></h3>
 			<a href="<?php echo site_url('dosen/showFormUpdateDosen/'.$dosen->id_dosen);?>">Edit</a> | 
 			<a href="<?php echo site_url('dosen/deleteDosen/'.$dosen->id_dosen);?>">Delete</a> <br><br>
-			nip : <?= $dosen->nip ?> <br>
-			alamat : <?= $dosen->alamat ?> <br>
+			nip : <?php echo $dosen->nip ?> <br>
+			alamat : <?php  $dosen->alamat ?> <br>
 		</div>
 	</li>
 <?php endforeach;?>
